d1da7daab811b939fdc88c0162be97c304661749
Keitel Jovin <keiteldog@gmail.com>
2023-07-03 20:23:57 -0400

    Put See All Projects link in place

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 import {
   GridContainer,
   GridItem,
 } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Container, Div1, Div2, Div3, NavLink, SocialIcons,
+} from '../Header/HeaderStyles';
+
 import ProjectItem from './ProjectItem';
 import prjs from '../../data/projects';
 
@@ -13,7 +18,12 @@ function Projects(props) {
   return (
     <Section id="projects">
       <SectionDivider />
-      <SectionTitle main>Projects (added: {prjs.length})</SectionTitle>
+      <SectionTitle main>Projects</SectionTitle>
+      {projects.length < prjs.length && (
+        <Link href="/projects" as="/projects">
+          <NavLink>See all Projects ({prjs.length})</NavLink>
+        </Link>
+      )}
       <GridContainer>
         {projects.map(project => (
           <GridItem key={project.id}>
--- src/data/accomplishments.js
+++ src/data/accomplishments.js
@@ -1,6 +1,7 @@
 export default [
   { number: 538100, text: '(Megalobiz) Monthly Visits' },
-  { number: 4660, text: 'StackOverflow reputation' },
+  { number: 4700, text: 'StackOverflow reputation' },
   { number: 1000, text: 'Testing Req/sec Load Balancing (10M DB records)' },
+  { number: 7, text: 'Years online of Megalobiz Website' },
   { number: 8, text: 'School Athletics medals (6🥇, 2🥈)' },
 ];
